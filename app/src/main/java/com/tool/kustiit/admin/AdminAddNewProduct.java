cd92710735309647f75d5d1817fa45e84e2700b7
Syeda <syed.ali.shah3938@gmail.com>
2019-05-18 11:47:44 +0500

    Admin Add Product Category

--- app/src/main/java/com/tool/kustiit/admin/AdminAddNewProduct.java
+++ app/src/main/java/com/tool/kustiit/admin/AdminAddNewProduct.java
@@ -2,9 +2,16 @@ package com.tool.kustiit.admin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProduct extends AppCompatActivity {
+
+    private Button addProductbtn;
+    private ImageView selectProductImg;
+    private EditText ProductName, ProductDescription, ProductPrice;
 
 
     private  String CategoryName ;
@@ -13,6 +20,12 @@ public class AdminAddNewProduct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
+
+
+        addProductbtn = (Button)findViewById(R.id.addProductBtn);
+        ProductName = (EditText)findViewById(R.id.productName);
+        ProductPrice = (EditText)findViewById(R.id.producPrice);
+        ProductDescription = (EditText)findViewById(R.id.productDescription);
 
         CategoryName = getIntent().getExtras().get("Catergory").toString();
         Toast.makeText(this, CategoryName, Toast.LENGTH_SHORT).show();
